cb0380e672e6e536dfef7966727266b44ded3489
gagscript-server2 <tiktokbusinessacc08@gmail.com>
2025-07-25 06:51:52 +0800

    Update and rename Summer-UPD_Initializer.lua to Initializer.lua

--- Initializer.lua
+++ Initializer.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/hoodratleahh/Summer-UPD/refs/heads/main/Initializer.lua"))()
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/2gYQVsLBUXUEn6Rl/raw", true))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/O54h1mDQzVKv0xcM/raw", true))()
